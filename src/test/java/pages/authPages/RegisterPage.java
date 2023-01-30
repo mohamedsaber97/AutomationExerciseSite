4a04c768df8d5c4d24374d0936a2f39c98fbea64
mohamedsaber97 <msaber9765@gmail.com>
2023-01-31 00:55:36 +0200

    create pom for register

--- src/test/java/pages/authPages/RegisterPage.java
+++ src/test/java/pages/authPages/RegisterPage.java
@@ -1,9 +1,8 @@
 package pages.authPages;
 
 import base.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 import java.io.IOException;
@@ -11,23 +10,21 @@ import java.io.IOException;
 public class RegisterPage extends TestBase {
 
     public RegisterPage() throws IOException {
-        PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "//*[@alt = 'Website for automation practice']")
-    WebElement homeLogo;
-
-    @FindBy(xpath = "//*[@href = '/login']")
-    WebElement signupLoginBtn;
-
+    By homeLogo = By.xpath("//*[@alt = 'Website for automation practice']");
+    By singUpLoginBtn = By.xpath("//*[@href = '/login']");
 
     public void checkLogo() {
-        boolean actualLogo = homeLogo.isDisplayed();
+        WebElement logo = driver.findElement(homeLogo);
+        boolean actualLogo = logo.isDisplayed();
         Assert.assertTrue(actualLogo, "-----the home logo is invisible-----");
         System.out.println("----home logo is visible-----");
     }
 
     public void registerTestCase() {
-        signupLoginBtn.click();
+        WebElement singUpLogin = driver.findElement(singUpLoginBtn);
+        singUpLogin.click();
     }
+
 }
--- src/test/java/testCases/authTestCases/RegisterTest.java
+++ src/test/java/testCases/authTestCases/RegisterTest.java
@@ -1,9 +1,6 @@
 package testCases.authTestCases;
 
 import base.TestBase;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,10 +18,7 @@ public class RegisterTest extends TestBase {
     @BeforeMethod
     public void openSiteAndVerifyHome() {
         initialization(properties.getProperty("browser"));
-        WebElement homeLogo = driver.findElement(By.xpath("//*[@alt = 'Website for automation practice']"));
-        boolean actualLogo = homeLogo.isDisplayed();
-        Assert.assertTrue(actualLogo, "-----the home logo is invisible-----");
-        System.out.println("----home logo is visible-----");
+        registerPage.checkLogo();
     }
 
     @AfterMethod
@@ -34,7 +28,6 @@ public class RegisterTest extends TestBase {
 
     @Test
     public void registerUserTC() {
-        WebElement btn = driver.findElement(By.xpath("//*[@href = '/login']"));
-        btn.click();
+        registerPage.registerTestCase();
     }
 }
